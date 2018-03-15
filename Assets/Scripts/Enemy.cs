5dddc40f17fdfe85442a20308a2bdb270b11a0a2
ranielagno <ranielagno@gmail.com>
2018-03-16 00:46:59 +0800

    Fix directory structuring for assets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject explosionFX;
+    [SerializeField] Transform parent;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        EnemyDestroy();
+    }
+	
+	private void EnemyDestroy()
+    {
+        GameObject explosion = Instantiate(explosionFX, transform.position, Quaternion.identity);
+        explosion.transform.parent = parent;
+        Destroy(gameObject);
+    }
+
+}
